a9ec7d8b556381630175f2c30e83bdb040878e0b
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-22 20:04:06 -0500

    created people-project contract instance

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-// enable web3 
-
-var web3 = new Web3(Web3.givenProvider);
+var web3 = new Web3(Web3.givenProvider); //enable web3 to interact with contract instance
+var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable();
+    window.ethereum.enable().then(function(accounts){ // permission to access Metamask account
+        contractInstance = new web3.eth.Contract(abi, "0xE069C5448F200b5E8ED17f5B9F4C67F06E485AB2", {from: accounts[0]}); // people-project contract
+        console.log(contractInstance);
+    }); 
+
+
+
+
+
 });
 
-// end enable web3
