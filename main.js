0def006dc22626f167f1cf47cbb9206175e53d2a
Rodrigo Ortega <ortegarod000@gmail.com>
2020-10-23 22:02:37 -0500

    imported people-project and successfully submitted tx to dev blockchain

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){ // permission to access Metamask account
-        contractInstance = new web3.eth.Contract(abi, "0xE069C5448F200b5E8ED17f5B9F4C67F06E485AB2", {from: accounts[0]}); // people-project contract
+        contractInstance = new web3.eth.Contract(abi, "0xb292f0B7Cf0763ADa448e13f69Ae2C371bbb4A61", {from: accounts[0]}); // people-project contract
         console.log(contractInstance);
     }); 
-
-
-
+    // add click handler using jQuery, executes function on click
+    $("#add_data_button").click(inputData)
 
 
 });
 
+
+function inputData(){
+    // alert("inputdata"); // to test
+    var name = $("#name_input").val();
+    var age = $("#age_input").val();
+    var height = $("#height_input").val();
+
+    var config = {
+        value: web3.utils.toWei("1", "ether")
+    }
+
+    contractInstance.methods.createPerson(name, age, height).send(config);
+}
